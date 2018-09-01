1158e303bdd64a46028115911f654783d6e14278
shadowolf <shadowolfbusiness@gmail.com>
2018-09-01 12:21:17 +1000

    Adding new memejsfork package and command.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const meme = require('memejs');
+const meme = require('memejsfork');
 const client = new Discord.Client({disableEveryone: true});
 
 
@@ -12,7 +12,7 @@ client.on('ready', () => {
   client.user.setActivity('Just a meme lol');
 
   function setActivity() {
-    const Gameinfo = ['Source: http://bit.ly/redditbotsource', 'Developer: shadowolf#9212', 'Discord: http://bit.ly/mancavediscord', 'Invite: http://bit.ly/inviterodney', `Running on ${client.guilds.size} ${pluralnonpluralservers}`, `Running for ${client.users.size} ${pluralnonpluralusers}`];
+    const Gameinfo = ['Source: http://bit.ly/redditbotsource', 'Developer: shadowolf#9212', 'Discord: http://bit.ly/mancavediscord', 'Invite: http://bit.ly/inviteredditbot', `Running on ${client.guilds.size} ${pluralnonpluralservers}`, `Running for ${client.users.size} ${pluralnonpluralusers}`];
     var info = Gameinfo[Math.floor(Math.random() * Gameinfo.length)];
 
     client.user.setActivity(info);
@@ -26,10 +26,12 @@ client.on('ready', () => {
     meme(function(data) {
       const embed = new Discord.RichEmbed()
       .setTitle(data.title[0])
+      .setURL(data.url[0])
+      .setDescription(`From ${data.subreddit[0]} by ${data.author[0]}`)
       .setImage(data.url[0])
       channel.send(embed).catch(console.error);
 
-      console.log('Meme has been posted.')
+      console.log('Meme has been posted.');
     })}, 600000);
 });
 
@@ -42,8 +44,10 @@ client.on('guildCreate', (guild) => {
   .setTitle('Cheers for the invite!')
   .setThumbnail('https://i.imgur.com/hWonxMb.png')
   .setColor('#ffa500')
-  .setDescription(`Thanks for inviting me to your Discord guild. I will be your meme provider. I will be posting memes in the #memes channel every 10 minutes. If you have not created a #memes channel, please do so.\n\nMy Developer is shadowolf#9212`)
+  .setDescription(`Thanks for inviting me to your Discord guild. I will be your meme provider. I will be posting memes in the #memes channel every 10 minutes.\nIf I haven't created a #memes channel, please do so.\n\nMy Developer is shadowolf#9212`)
   guild.channels.find(`name`,`general`).send(embed);
+
+  guild.createChannel(`memes`, `text`).catch(console.error);
 });
 
 
@@ -66,10 +70,36 @@ client.on('message', (message) => {
     meme(function(data) {
       const embed = new Discord.RichEmbed()
       .setTitle(data.title[0])
+      .setURL(data.url[0])
+      .setDescription(`From r/${data.subreddit[0]} by ${data.author[0]}`)
       .setImage(data.url[0])
       message.channel.send({embed});
     });
   };
+
+  if (command === "redditbroadcast") {
+    if (message.author.id === '169978063478587392') {
+      meme(function(data) {
+        const embed = new Discord.RichEmbed()
+        .setTitle(data.title[0])
+        .setURL(data.url[0])
+        .setDescription(`From r/${data.subreddit[0]} by ${data.author[0]}`)
+        .setImage(data.url[0])
+
+      const guildList = client.guilds.array();
+      try {
+        guildList.forEach(guild => guild.channels.find("name", "memes").send({embed}));
+      } catch (err) {
+        console.log("Could not send meme blast\n" + err);
+      }
+    })
+
+    const completeembed = new Discord.RichEmbed()
+    .setTitle('Reddit Broadcast Complete')
+    message.channel.send(completeembed);
+  } else  {
+    console.log('No can do boss.');
+  }};
 });
 
 client.login(process.env.BOT_TOKEN);
